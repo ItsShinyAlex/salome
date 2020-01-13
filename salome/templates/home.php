64e50b51fa893e50e28868d8d91467adea025fdb
Lyngall <37223445+ItsShinyAlex@users.noreply.github.com>
2020-01-13 13:22:40 -0600

    avance

---
+++ salome/templates/home.php
@@ -0,0 +1,195 @@
+<?php
+    /* Template Name: Principal */ 
+    get_header();
+echo "<section id ='shiny-salome' class='shiny-template' >";
+?>
+
+
+    <section id="mexicana" class="template-section">
+        <div class="contain">
+          <div class="fcentered">
+              
+              <div class=" center">
+                  
+                  <h1><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/txtslide.png" alt="Es mexicana, es divina, es única y cercana a la perfección, se llama Salomé"></h1>
+                  
+              </div>
+              
+          </div>
+        </div>
+    </section>
+    <div class="fondo-figura"></div>
+    
+    <section id="seducir" class="template-section">
+        <div class="contain">
+         <div class="flex">
+             
+             <div class="col txt">
+                 
+                <h2><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/seducir.png" alt="Dejate seducir por Salome"> </h2>
+                <p>
+                    
+                    Su nombre significa <b>"aquella cercana a la perfección"</b> y así es Salomé, joven, misteriosa, interesante, delicada, deliciosa, una buena compañía en todo lugar.
+                     <br><br>
+                    Déjate seducir por su frescura, sabor y alma <b class="brown">100% mexicana.</b>
+                    
+                </p>
+                
+                <a href="" class="btn-salome">Saber más</a>
+                
+                 
+             </div>
+             <div class="col cerveza fcentered">
+                 <div class="center"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/botella.png" alt="cerveza salome"></div>
+                 
+                 
+             </div>
+             
+         </div>
+        </div>
+    </section>
+    
+        <section id="beer" class="template-section">
+        <div class="contain">
+         <div class="center fcentered">
+             
+             
+             <div class="center fcentered">
+                 
+                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/aliada.png" alt="Salome es la perfecta aliada para tu antro, restaurant o taqueria">
+                <a href="" class="btn-salome">Saber más</a>
+                 
+             </div>
+
+             
+         </div>
+        </div>
+    </section>
+    
+    
+      <section id="alfaro" class="template-section">
+        <div class="contain">
+         <div class="flex ">
+       
+                         <div class="col txt center fcentered">
+
+                        <h2><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/alfaro.png" alt="Cerveceria Alfaro"> </h2>
+                        <p>
+
+                            En <b>Cervecería Alfaro</b> los mejores momentos donde se pueda apreciar el gusto, sensación y experiencia de una <b class="brown">cerveza 100% mexicana</b> de alta calidad con inigualable sabor <span class="brown">¡Conócenos!</span>
+ 
+
+                        </p>
+
+                        <a href="" class="btn-salome">Saber más</a>
+
+
+                     </div>
+                     <div class="col cerveza fcentered">
+                         <div class="center"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/botellas.png" alt="botellas cerveza"></div>
+
+
+                     </div>
+             
+         </div>
+        </div>
+    </section>
+    
+    <section id="contacto" class="template-section">
+            <div class="contain">
+            
+            <div class="center">
+                
+                <h2><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/contactanos.png" alt="Contáctanos"></h2>
+                <p class="brown">
+                    Lo realmente mexicano, Cerveza Salomé. 
+                    Aquella que esta cercana a la perfección.
+                </p>
+                
+            </div>
+            
+             <div class="flex ">
+
+                    <div class="col" id="form-col">
+
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/logo.png" alt="cerveza salome">
+                            <?php echo do_shortcode('[caldera_form id="CF5d8051a0804fa"]');?>
+                            
+
+                    </div>
+                    <div class="col map fcentered">
+                            
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1878.2894827669263!2d-99.17329247664334!3d19.68796015604988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f5693b73888f%3A0x5f6716b449d7b69b!2sAv.%20San%20Roque%2C%20Fracci%C3%B3n%20San%20Roque%2C%20M%C3%A9x.!5e0!3m2!1ses-419!2smx!4v1577207650914!5m2!1ses-419!2smx" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+
+                    </div>
+
+             </div>
+             
+             
+             <div class="flex">
+                 
+                 <div class="col c25 center">
+                     
+                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/phone.png" alt="phone">
+                     
+                     <p>55 5555 5555</p>
+                     
+                 </div>
+                 
+                 <div class="col c25 center">
+                     
+                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/whatsapp.png" alt="hwhatsapp">
+                     
+                     <p>55 5555 5555</p>
+                     
+                 </div>
+                 
+                 <div class="col c25 center">
+                     
+                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/location.png" alt="location">
+                     
+                     <p>Camino a San Mateo 1
+                        Col. San Roque
+                        Cuautitlán Izcalli 54820</p>
+                     
+                 </div>
+                 
+                 <div class="col c25 center">
+                     
+                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/mail.png" alt="mail">
+                     
+                     <p>contacto@cervezasalome.com</p>
+                     
+                 </div>
+                 
+             </div>
+             
+             
+            
+             
+            </div>
+    </section>
+    
+    <section id="networks">
+        
+        <div class="contain">
+            
+             <div class="center">
+                 
+                 <p id="redes"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/facebook.png" alt="facebook"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/instagram.png" alt="instagram">/CervezaSalomé  </p>
+                 
+             </div>
+            
+        </div>
+        
+    </section>
+
+    
+
+
+
+
+    <?php 
+    get_footer();
+echo "</section>";
+?>
